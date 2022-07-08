5d73195754d935729aea626fe61d3f2ce2e2d10f
Alexey Romanov <romanov.alexey2000@gmail.com>
2022-07-08 14:26:05 +0300

    lib: move to allocator folder

--- allocator/dalloc.c
+++ allocator/dalloc.c
@@ -1,10 +1,10 @@
 #include "dalloc.h"
 
-#define DMALLOC_ALIGN 4
-
 #include <stdint.h>
 #include <sys/mman.h>
 #include <stdbool.h>
+
+#include "list.h"
 
 #ifdef DEBUG
 #include <stdio.h>
@@ -14,78 +14,82 @@ typedef struct dalloc_header {
     uint16_t size;
 } dalloc_header_t;
 
-typedef struct dalloc_block dalloc_block_t;
 typedef struct dalloc_block {
     dalloc_header_t hdr;
     void *mem;
 } dalloc_block_t;
 
-typedef struct dalloc_free_blocks dalloc_free_block_t;
 typedef struct dalloc_free_blocks {
     dalloc_block_t *block;
-    dalloc_free_block_t *next;
+    struct list_head node;
 } dalloc_free_block_t;
 
-typedef struct dalloc_chunk dalloc_chunk_t;
-struct dalloc_chunk {
-#define DMALLOC_CHUNK_SIZE 1024
-
+typedef struct dalloc_chunk {
+#define DALLOC_CHUNK_SIZE 1024
     void *start;
     void *end;
 
     dalloc_block_t *maxblock;
-    dalloc_free_block_t *freeblocks;
+    dalloc_free_block_t freeblocks;
 
-    dalloc_chunk_t *next;
-    /* dalloc_chunk_t *prev; */
+    struct list_head node;
+} dalloc_chunk_t;
+
+typedef struct dalloc_work {
+    bool is_init;
+    dalloc_chunk_t *chunk_head;
+} dalloc_work_t;
+
+static dalloc_work_t __dalloc = {
+    .is_init = false,
+    .chunk_head = NULL,
 };
 
-bool init = false;
-dalloc_chunk_t *chunk_head = NULL;
-
-#define dalloc_mmap(size) \
+#define MMAP(size) \
     mmap(NULL, (size), PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0)
 
+/*
 #define dalloc_get_memblock(mem) \
     (dalloc_block_t *) mem - sizeof(dalloc_block_t)
 
 #define dalloc_block_end(block, size) \
     (char *)((char *)block + sizeof(dalloc_block_t) + size)
+    */
 
-#define dalloc_mem_offset(block) \
-    block + sizeof(dalloc_block_t)
+#define BLOCK_MEM_OFFSET(block) \
+    (char *)block + sizeof(dalloc_block_t)
 
-static inline dalloc_block_t *create_block(uint16_t cnt) {
+#define BLOCK_GET_MEM(mem) \
+    (dalloc_block_t*)((char *)mem - sizeof(dalloc_block_t))
+
+static inline dalloc_block_t *create_block(uint16_t block_size) {
     dalloc_block_t *block = \
-        dalloc_mmap(sizeof(dalloc_block_t) + cnt * sizeof(char));
+        MMAP(sizeof(dalloc_block_t) + block_size * sizeof(char));
 
-    block->mem = dalloc_mem_offset(block);
-    block->hdr.size = cnt;
+    block->mem = BLOCK_MEM_OFFSET(block);
+    block->hdr.size = block_size;
 
     return block;
 }
 
 static dalloc_chunk_t *create_chunk(void) {
-    dalloc_chunk_t *chunk = dalloc_mmap(sizeof(dalloc_chunk_t));
-    chunk->freeblocks = dalloc_mmap(sizeof(dalloc_free_block_t));
-    chunk->maxblock = create_block(DMALLOC_CHUNK_SIZE);
+    dalloc_chunk_t *chunk = MMAP(sizeof(dalloc_chunk_t));
+
+    chunk->maxblock = create_block(DALLOC_CHUNK_SIZE);
+    chunk->freeblocks.block = chunk->maxblock;
+    INIT_LIST_HEAD(&(chunk->freeblocks.node));
 
     chunk->start = chunk->maxblock;
-    chunk->end = chunk->maxblock + DMALLOC_CHUNK_SIZE;
-
-    chunk->freeblocks->block = chunk->maxblock;
-    chunk->freeblocks->next = NULL;
-
-    chunk->next = NULL;
-    /* chunk->prev = NULL; */
+    chunk->end = (char*)chunk->maxblock + DALLOC_CHUNK_SIZE;
 
     return chunk;
 }
 
 __attribute__((unused)) static void free_chunk(dalloc_chunk_t *chunk) {
-    munmap(chunk, sizeof(dalloc_chunk_t) + DMALLOC_CHUNK_SIZE);
+    munmap(chunk, sizeof(dalloc_chunk_t) + DALLOC_CHUNK_SIZE);
 }
 
+#if 0
 static void update_maxblock(dalloc_chunk_t **chunk) {}
 
 #define dalloc_remove_block(chunk, b) do { \
@@ -141,45 +145,52 @@ static dalloc_block_t *get_freeblock(dalloc_chunk_t **chunk, uint16_t size) {
 
     return free_block;
 }
+#endif
 
-static void dmalloc_init(void) {
-    chunk_head = create_chunk();
-    init = true;
+static void dalloc_init(void) {
+    __dalloc.chunk_head = create_chunk();
+    INIT_LIST_HEAD(&(__dalloc.chunk_head->node));
+    __dalloc.is_init = true;
 }
 
 void *dalloc(size_t cnt) {
-    if (!init)
-        dmalloc_init();
+    dalloc_chunk_t *curr_chunk = NULL, *tmp = NULL;
+    dalloc_block_t *block = NULL;
 
-    dalloc_chunk_t *curr_chunk = chunk_head;
-    while (curr_chunk->maxblock->hdr.size < cnt) {
-        printf("iterate..\n");
+    if (__dalloc.chunk_head == NULL)
+        dalloc_init();
 
-        if (curr_chunk->next == NULL)
-            curr_chunk->next = create_chunk();
-
-        curr_chunk = curr_chunk->next;
+    curr_chunk = __dalloc.chunk_head;
+    list_for_each_entry(curr_chunk, &(curr_chunk->node), node) {
+        if (curr_chunk->maxblock->hdr.size >= cnt)
+            break;
     }
 
-    dalloc_block_t *block = get_freeblock(&curr_chunk, cnt);
+    (void)tmp;
+    //tmp = create_chunk();
+    //list_add(&(curr_chunk->node), &(tmp->node));
+    block = curr_chunk->maxblock;
+
+    //block = get_freeblock(&curr_chunk, cnt);
 
 #ifdef DEBUG
-    printf("%s:%d header size", __func__, block->hdr.size);
+    printf("alloc --->: %s:%d header size, ptrs: %p, %p\n", __func__, block->hdr.size, block, block->mem);
 #endif
 
     return block->mem;
 }
 
-void dfree(void *mem) {
-    dalloc_block_t *block = dalloc_get_memblock(mem);
+void dfree(void *memblock) {
+    dalloc_block_t *block = BLOCK_GET_MEM(memblock);
 
 #ifdef DEBUG
-    printf("%s:%p | %p, %zu\n\n", __func__, mem, block, sizeof(dalloc_header_t));
+    printf("ptrs: %p, %p\n", block, memblock);
     for (int i = 0; i < 128; i++) {
         int *b = (int*)block->mem;
-        printf("%d\n", b[i]);
+        // printf("%d\n", b[i]);
     }
-    printf("suze:%d, block pouinter:%p", block->hdr.size, block);
+
+    printf("free ---> size:%d, block pointer:%p\n", block->hdr.size, block);
 #endif
 
     munmap(block->mem, block->hdr.size);
--- allocator/main.c
+++ allocator/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "dalloc.h"
+#include "list.h"
 
 int main(int argc, char **argv) {
     setbuf(stdout, NULL);
+
+    struct list_head list;
+    INIT_LIST_HEAD(&list);
 
     int *ptr = dalloc(128 * sizeof(int));
 
@@ -10,13 +14,16 @@ int main(int argc, char **argv) {
         ptr[i] = rand();
         // printf("%d\n", ptr[i]);
     }
+    printf("====\n");
 
+#if 0
     int *ptr1 = dalloc(50 * sizeof(int));
 
     for (int i = 0; i < 50; i++) {
         ptr1[i] = rand();
         printf("%d\n", ptr[i]);
     }
+#endif
 
     dfree(ptr);
 
